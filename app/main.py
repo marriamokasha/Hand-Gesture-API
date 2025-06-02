339bd31cf240dbf084a0c06cc845c43b0cf66a22
Mariam Okasha <55918119+marriamokasha@users.noreply.github.com>
2025-06-03 01:06:50 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -65,4 +65,4 @@ def predict(data: InputFeatures):
         return {"error": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
